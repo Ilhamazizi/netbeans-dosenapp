fe8657611a0562777ef37071fb2774ce19bd19dd
Ilhamazizi <muh.ilhamazizi@gmail.com>
2022-10-05 09:19:48 +0700

    connection

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple lecturer CRUD application (lecture assignments).
---
+++ src/com/app/connection/DBConnection.java
@@ -0,0 +1,31 @@
+
+package com.app.connection;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author casper
+ */
+public class DBConnection {
+    private static Connection conn = null;
+    
+    static {
+        if (conn == null) {
+            MysqlDataSource ds = new MysqlDataSource();
+            ds.setServerName("localhost");
+            ds.setDatabaseName("akademik");
+            ds.setUser("azeezee");
+            ds.setPassword("123");
+            
+            try {
+                conn = ds.getConnection();
+                
+            } catch (SQLException ex) {
+                
+            }
+        }
+    }
+}
